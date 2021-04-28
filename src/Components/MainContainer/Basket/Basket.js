97f4b16ff982f1f4d3e73f5c9fd13d7c620bc782
meghnavarma0 <meghnavarma0@gmail.com>
2021-04-28 16:07:01 +0530

    Added minus icon plus functionality

--- src/Components/MainContainer/Basket/Basket.js
+++ src/Components/MainContainer/Basket/Basket.js
@@ -1,25 +1,47 @@
 import React, { useState } from "react";
 import basketImg from "../../../assets/Images/black-shopping-basket.png";
+import dustbinIcon from "../../../assets/Images/dustbin.png";
+import minusIcon from "../../../assets/Images/minus.png";
 import Item from "../Item/Item";
 import classes from "./Basket.module.css";
 
-const Basket = () => {
-  const basketList = ["apple", "banana", "mango"];
+const Basket = (props) => {
   return (
     <div>
-      <header className={classes.Heading}>
-        <img className={classes.Img} src={basketImg} alt="basket" />
-        <b>Basket</b>
-      </header>
+      <div className={classes.Container}>
+        <header className={classes.Heading}>
+          <img className={classes.Img} src={basketImg} alt="basket" />
+          <b>Basket</b>
+        </header>
+
+        <img
+          onClick={props.onDustbinClickedHandler}
+          className={classes.Dustbin}
+          src={dustbinIcon}
+          alt="dustbin"
+        />
+      </div>
+
       <ul style={{ listStyleType: "none" }}>
-        {basketList.map((item, index) => {
-          const bgc = index % 2 === 0 ? "#f9f9f9" : "#efefef";
-          return (
-            <Item key={index} bgc={bgc}>
-              {item}
-            </Item>
-          );
-        })}
+        {props.bucketList.length > 0 ? (
+          props.bucketList.map((item, index) => {
+            const bgc = index % 2 === 0 ? "#f9f9f9" : "#efefef";
+
+            return (
+              <Item
+                key={index}
+                bgc={bgc}
+                onClickHandler={props.onBasketClickHandler}
+                value={item.id}
+                icon={minusIcon}
+              >
+                {`${item.count} ${item.grocery}`}
+              </Item>
+            );
+          })
+        ) : (
+          <Item>Your basket is empty!</Item>
+        )}
       </ul>
     </div>
   );
