b2e798c98921440675f972fe79270ea4d36b6196
meghnavarma0 <meghnavarma0@gmail.com>
2021-04-28 16:06:15 +0530

    Added SearchBar in main container

--- src/Components/MainContainer/MainContainer.js
+++ src/Components/MainContainer/MainContainer.js
@@ -1,13 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./MainContainer.module.css";
 import Basket from "./Basket/Basket";
 import Groceries from "./Groceries/Groceries";
+import SearchBar from "../SearchBar/SearchBar";
+
+const groceryList = [
+  "Strawberry",
+  "Blueberry",
+  "Orange",
+  "Banana",
+  "Apple",
+  "Carrot",
+  "Celery",
+  "Mushroom",
+  "Green Pepper",
+  "Eggs",
+  "Cheese",
+  "Butter",
+  "Chicken",
+  "Beef",
+  "Pork",
+  "Fish",
+  "Rice",
+  "Pasta",
+  "Bread",
+];
 
 const MainContainer = () => {
+  const [bucketList, setBucketList] = useState([]);
+  const [grList, setGrList] = useState(groceryList);
+  const onGroceryClickHandler = (id) => {
+    let newList = [];
+    let flag = true;
+    for (let i = 0; i < bucketList.length; i++) {
+      let newVal = bucketList[i];
+      if (bucketList[i].id === id) {
+        newVal.count += 1;
+        flag = false;
+      }
+
+      newList.push(newVal);
+    }
+    if (flag) {
+      newList.push({
+        id,
+        count: 1,
+        grocery: groceryList[id],
+      });
+    }
+    setBucketList(newList);
+  };
+
+  const onBasketClickHandler = (id) => {
+    let newList = [];
+
+    for (let i = 0; i < bucketList.length; i++) {
+      let newVal = bucketList[i];
+      if (bucketList[i].id === id) {
+        newVal.count -= 1;
+      }
+
+      if (newVal.count === 0) {
+        continue;
+      }
+
+      newList.push(newVal);
+    }
+
+    setBucketList(newList);
+  };
+
+  const onDustbinClickedHandler = () => {
+    setBucketList([]);
+  };
+
+  const onInputChangedHandler = (e) => {
+    const val = e.target.value.trim();
+    const newGrList = grList.filter(
+      (item) => item.toLowerCase().slice(0, val.length) === val.toLowerCase()
+    );
+    if (val === "") {
+      setGrList(groceryList);
+      return;
+    }
+    setGrList(newGrList);
+  };
+
   return (
-    <div className={classes.MainContainer}>
-      <Groceries />
-      <Basket />
+    <div>
+      <SearchBar onInputChangedHandler={onInputChangedHandler} />
+      <div className={classes.MainContainer}>
+        <Groceries
+          groceryList={grList}
+          onGroceryClickHandler={onGroceryClickHandler}
+        />
+        <Basket
+          bucketList={bucketList}
+          onBasketClickHandler={onBasketClickHandler}
+          onDustbinClickedHandler={onDustbinClickedHandler}
+        />
+      </div>
     </div>
   );
 };
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./SearchBar.module.css";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const [item, setItem] = useState("");
   return (
     <div className={classes.SearchBar}>
       <form action="submit">
-        <input className={classes.Input} type="text" placeholder="Search" />
+        <input
+          onChange={(e) => {
+            setItem(e.target.value);
+            props.onInputChangedHandler(e);
+          }}
+          className={classes.Input}
+          type="text"
+          placeholder="Search Item"
+          value={item}
+        />
       </form>
     </div>
   );
