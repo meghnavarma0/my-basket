c54a22fc76540ddaa01eb924ff458a9874070e28
meghnavarma0 <meghnavarma0@gmail.com>
2021-04-28 16:07:29 +0530

    added plus icon and functionality

--- src/Components/MainContainer/Groceries/Groceries.js
+++ src/Components/MainContainer/Groceries/Groceries.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import groceryImg from "../../../assets/Images/grocery.png";
 import Item from "../Item/Item";
 import classes from "./Groceries.module.css";
+import plusIcon from "../../../assets/Images/plus-sign.png";
 
-const Groceries = () => {
-  const groceryList = ["apple", "banana", "mango", "grapes", "eggs"];
+const Groceries = (props) => {
   return (
     <div>
       <header className={classes.Heading}>
@@ -12,10 +12,16 @@ const Groceries = () => {
         <b>Groceries</b>
       </header>
       <ul style={{ listStyleType: "none" }}>
-        {groceryList.map((item, index) => {
+        {props.groceryList.map((item, index) => {
           const bgc = index % 2 === 0 ? "#f9f9f9" : "#efefef";
           return (
-            <Item key={index} bgc={bgc}>
+            <Item
+              key={index}
+              value={index}
+              bgc={bgc}
+              onClickHandler={props.onGroceryClickHandler}
+              icon={plusIcon}
+            >
               {item}
             </Item>
           );
