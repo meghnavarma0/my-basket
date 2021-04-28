319abc8879c67e1aae6fa460584524217a753389
meghnavarma0 <meghnavarma0@gmail.com>
2021-04-28 18:46:30 +0530

    removed form tag

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,18 +5,16 @@ const SearchBar = (props) => {
   const [item, setItem] = useState("");
   return (
     <div className={classes.SearchBar}>
-      <form action="submit">
-        <input
-          onChange={(e) => {
-            setItem(e.target.value);
-            props.onInputChangedHandler(e);
-          }}
-          className={classes.Input}
-          type="text"
-          placeholder="Search Item"
-          value={item}
-        />
-      </form>
+      <input
+        onChange={(e) => {
+          setItem(e.target.value);
+          props.onInputChangedHandler(e);
+        }}
+        className={classes.Input}
+        type="text"
+        placeholder="Search Item"
+        value={item}
+      />
     </div>
   );
 };
